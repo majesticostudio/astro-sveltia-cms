4ab24ce7dd5508c0c72ea8b536815f8cdc534317
Giulio Zanchetta <zank@outlook.it>
2023-11-25 12:59:35 +0100

    Add Cloudflare adapter to demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,9 +1,12 @@
 import sveltiaCms from "../dist/astro-sveltia-cms";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astro-sveltia-cms.vercel.app",
 	integrations: [sveltiaCms()],
 	output: "server",
+	adapter: cloudflare(),
 });
